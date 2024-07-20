233fd7e89e5419a26098970b006d26a65df66d66
Donlekao <alexandre200970@hotmail.com>
2024-07-20 00:18:48 -0300

    teste20.07

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -1,9 +1,40 @@
 
+import Cabecalho from "../components/cabecalho";
+
 function Sobre(){
     return(
-        <div>
+        <div className="container_sobre">
+            <Cabecalho/>
+
+            <div>
+
             <h1>Pagina Sobre</h1>
+            <h3>Sangue bom é um site projetado em informar as pessoas sobre doações de sangue, onde doar, quem pode doar, locais de doação e tambem informativo a respeito.</h3>
+            <br />
+            <ul>
+                <h2>Criadores</h2>
+            <li>Alexandre</li>
+            25 anos, e nunca doou sangue, certo dia me bateu a curiosidade e coragem para fazer a doação
+            porem não achava informações faceis de como e onde realizar, então fui atras e descobri que 
+            não estava apto naquele momento.
+            <li>Daniel</li>
+            25 anos, e nunca doou sangue, certo dia me bateu a curiosidade e coragem para fazer a doação
+            porem não achava informações faceis de como e onde realizar, então fui atras e descobri que 
+            não estava apto naquele momento.
+            <li>Gabriel</li>
+            25 anos, e nunca doou sangue, certo dia me bateu a curiosidade e coragem para fazer a doação
+            porem não achava informações faceis de como e onde realizar, então fui atras e descobri que 
+            não estava apto naquele momento.
+            <li>Gustavo</li>
+            25 anos, e nunca doou sangue, certo dia me bateu a curiosidade e coragem para fazer a doação
+            porem não achava informações faceis de como e onde realizar, então fui atras e descobri que 
+            não estava apto naquele momento.
+            </ul>
+
+            </div>
+        
         </div>
+
     )
 }
 
