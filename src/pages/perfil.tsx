c22e1ff1b8664d127d6137d6d0ce188fe3225d27
Hyan Moreno <hyanhenrique09@gmail.com>
2024-08-16 22:23:02 -0300

    inicei a pagina de perfil

--- src/pages/perfil.tsx
+++ src/pages/perfil.tsx
@@ -1,10 +1,30 @@
+import Cabecalho from "../components/cabecalho";
 import { TabTitle } from "../components/GeneralFunctions/GeneralFunctions";
 
 function Perfil(){
     TabTitle('Perfil - Sangue Bom')
     return(
         <div>
-            <h1>Pagina de perfik</h1>
+            <Cabecalho/>
+                <div className="perfil_principal">
+                    <div className="titulo_imagem_botaocertificado">
+                        Editar Perfis <br />
+                        <h4>Helena Hills</h4>
+
+                        </div>
+                    <div className="dados_perfil">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="botao_vermelho">
+
+                    </div>
+                </div>
+
+
+
+
         </div>
     )
 }
