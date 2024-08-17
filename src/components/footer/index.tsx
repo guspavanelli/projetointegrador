724ba51c6b6dc135bde34982ebdb7ac4846ea22c
Daniel Izidoro <daniizidoro05@gmail.com>
2024-08-16 22:28:13 -0300

    Alterações footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,29 +1,33 @@
+import Instagram from "../../assests/footer/instagram (2).png"
+import Facebook from "../../assests/footer/facebook.png"
+import Linkedin from "../../assests/footer/linkedin.png"
+import Youtube from "../../assests/footer/youtube.png"
+
 function Footer(){
     return (
     <div className="footer_principal">
-        <div className="footer_apo">
+        <div className="footer_1">
             <h1 className="title_footer">Apoiadores</h1>
-            <h2>Seja um Apoiador!</h2>
-            <h4>Sobre o Sangue-Bom</h4>
+            <h2 className="subtitle_footer">Seja um Apoiador!</h2>
+            <h4 className="subtitle_footer">Sobre o Sangue-Bom</h4>
+        <div>
+            <img src = {Instagram} alt="Instagram"/>
+            <img src = {Facebook} alt="Facebook" />
+            <img src= {Linkedin} alt="Linkedin" />
+            <img src= {Youtube} alt="Youtube" />
         </div>
-        <div className="footer_sobr">
-            <h2>Sobre nós</h2>
-            <h4>Contatos</h4>
-            <h4>Nossa História</h4>
         </div>
-        <div className="footer_duv">
-            <h2>Dúvidas frequentes</h2>
-            <h4>Teste de Aptidão</h4>
-            <h4>Chekin</h4>
+        <div className="footer_2">
+            <h2 className="subtitle_footer">Sobre nós</h2>
+            <h4 className="subtitle_footer">Contatos</h4>
+            <h4 className="subtitle_footer">Nossa História</h4>
         </div>
-        <div className="footer_Loc">
-            <h2>Locais Para doação</h2>
-            <h4>Hemonúcleos</h4>
-            <h4>Mapa</h4>
+        <div className="footer_3">
+            <h2 className="subtitle_footer">Dúvidas frequentes</h2>
+            <h4 className="subtitle_footer">Teste de Aptidão</h4>
+            <h4 className="subtitle_footer">Chekin</h4>
         </div>
-        <div className="footer_qr">
-            <h2>Ajude-nos</h2>
-        </div>
+        
     </div>
     )
 }
