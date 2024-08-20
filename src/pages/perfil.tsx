040a3db11424dd60a1e483a67ce5603e07cef917
Hyan Moreno <hyanhenrique09@gmail.com>
2024-08-19 21:57:17 -0300

    perfil

--- src/pages/perfil.tsx
+++ src/pages/perfil.tsx
@@ -1,25 +1,33 @@
 import Cabecalho from "../components/cabecalho";
 import { TabTitle } from "../components/GeneralFunctions/GeneralFunctions";
+import '../style/style.css';
+import ftPerfil from "../assests/usuario-de-perfil.png"
 
 function Perfil(){
     TabTitle('Perfil - Sangue Bom')
     return(
         <div>
             <Cabecalho/>
-                <div className="perfil_principal">
+                <div className="perfil_pr">
                     <div className="titulo_imagem_botaocertificado">
                         Editar Perfis <br />
                         <h4>Helena Hills</h4>
+                             <div className="imgp"> <img src={ftPerfil} alt="foto perfil"/></div>
 
-                        </div>
-                    <div className="dados_perfil">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
+                     </div>
+
+
+
+
+
+                             <div className="dados_perfil">
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
                     <div className="botao_vermelho">
 
-                    </div>
+                         </div>
                 </div>
 
 
