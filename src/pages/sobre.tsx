5a1451161dad040f367f36b7eaf4cfd0418af178
Leko <alexandre200970@hotmail.com>
2024-08-23 21:59:14 -0300

    23.08leko

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -1,6 +1,7 @@
 import { TabTitle } from "../components/GeneralFunctions/GeneralFunctions";
 import Footer from "../components/footer";
 import Cabecalho from "../components/cabecalho";
+import QrCode from "../assests/qrcode.webp"
 
 function Sobre(){
     TabTitle('Sobre - Sangue Bom')
@@ -11,6 +12,7 @@ function Sobre(){
             <div>
 
             <h1>Pagina Sobre</h1>
+            <br />
             <h3>Sangue bom é um site projetado em informar as pessoas sobre doações de sangue, onde doar, quem pode doar, locais de doação e tambem informativo a respeito.</h3>
             <br />
 
@@ -29,8 +31,26 @@ function Sobre(){
                     não estava apto naquele momento</li>
                 <li>Gustavo, 25 anos e nunca doou sangue, certo dia me bateu a curiosidade e coragem para fazer a doação
                     porem não achava informações faceis de como e onde realizar, então fui atras e descobri que 
-                    não estava apto naquele momento</li>
+                    não estava apto naquele momento.</li>
+                    <br />
+                    <br />
+
+
+                    <div className="sobre_doacao_">
+                    Ajude-nos e Seja um Sanguebom!
+                    <br />
+                    Ajude a manter o projeto e seja um membro Sangue Bom!
+                    Faça sua doação via PIX para os seguintes dados:
+                    <br />
+                    <br />
+                    <br />
                     
+                        <div className="pai_qr">
+                               <div className="info_qr"> CNPJ: XXXXXXXXXXXXXXXXXXXX Nome: XXXXXXXXXXXXXXXXXXXX</div>
+                                    <div className="sobre_qr_">
+                                        <img className="qr_sb" src={QrCode} alt="qr code" /></div>
+                        </div>
+                    </div>
             </ul> 
 
             </div>
