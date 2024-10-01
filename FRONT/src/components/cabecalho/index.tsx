ba01ec1d048fc43c9be9700063f077aa1c5fd5fa
ivan1234-esta <you@example.com>
2024-09-30 22:20:16 -0300

    atualização 30/09

--- FRONT/src/components/cabecalho/index.tsx
+++ FRONT/src/components/cabecalho/index.tsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import Botao from "../../components/buttons/login/index";
+import img_logo from "../../assests/logomarca-sanguebom.png"; 
+
 
 function Cabecalho(){
     return(
         <div className="cab_container">
-            <Link className="cab_logo" to="/">
-            <div >
-                SANGUE BOM
-            </div>
+            <Link to="/">
+            <img className='img_logo' src={img_logo} title="logomarca SangueBom" alt="Logomarca SangueBom" />
                 </Link> 
             <div className="cab_options">
                 <Link className="cab_options_home"to="/locais"><div >Locais</div></Link>
