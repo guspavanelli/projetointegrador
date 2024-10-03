a9a415ae64f18d070fa7880f5342a7e892bcbd9b
ivan1234-esta <ivandavidchagas@gmail.com>
2024-10-02 22:03:48 -0300

    atualizacao

--- FRONT/src/pages/home.tsx
+++ FRONT/src/pages/home.tsx
@@ -139,9 +139,7 @@ function Home() {
 
         return (
             <div className="quiz-container">
-                 <div className="image">
-                        <img style={{padding: '0px',width:'350px'}} src={imagemteste} alt="img doação SangueBom" />
-                    </div>
+                        <img style={{padding: '0px', width:'50%'}}  src={imagemteste} alt="img doação SangueBom" />
                 {modalVisible && (
                     <div className="modal">
                         <div className="modal-content">
@@ -270,12 +268,9 @@ function Home() {
         }
     
         return (
-            <div>
                 <div className="container">
                     <div className="form-wrapper">
-                        <div className="image">
-                            <img style={{padding: '0px',width:'auto'}}  src={imagemcadastro} alt="img doação SangueBom" />
-                        </div>
+                            <img style={{padding: '0px', width:'50%'}}  src={imagemcadastro} alt="img doação SangueBom" />
                         <div className="form">
                             <h3 className="tit_cads">Seja Bem-vindo(a)!<br />Cadastre-se aqui:</h3>
                             <p className="sub_tit_cads">Digite seus dados para criar sua conta:</p>
@@ -307,16 +302,15 @@ function Home() {
                                 </div>
                             )}
                         </div>
-                        <div className="retorno">
+                        {/* <div className="retorno">
                             <a href="/" className="a_retorno">
                                 <svg className="angle-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                                     <path fill="#ff7f7f" d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/>
                                 </svg>
                             </a>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
-            </div>
         );
     }
     return (
