62f1813d4cda438ae6d04490b7f2f968052859d3
Daniel Izidoro <daniizidoro05@gmail.com>
2024-10-10 22:10:50 -0300

    test

--- FRONT/src/pages/sobre.tsx
+++ FRONT/src/pages/sobre.tsx
@@ -10,7 +10,6 @@ function Sobre(){
     return(
         <div className="container_sobre_">
             <Cabecalho/> 
-<<<<<<< HEAD
             <br />
             <br />
             <div className="title_sobre">
@@ -38,38 +37,6 @@ function Sobre(){
 
 
                         <div className="projeto-box1">
-=======
-            <h1>Sobre o sangue bom</h1>
-            <br />
-            <h3>Sangue bom é um site projetado em informar as pessoas sobre doações de sangue, onde doar, quem pode doar, locais de doação e tambem informativo a respeito.</h3>
-            <br />
-            <p>Criadores</p>
-            <CardList/>
-            <ul>
-                <li>Alexandre, gosto muito de esportes e computadores, comecei o curso com o objetivo de fazer um site para minha
-                    mei de camisas de time e tambem por ser uma área que tem um grande mercado de trabalho que eu pretendo seguir ...
-                </li>
-                    <br />
-                        <li>Daniel, atualmente analistas de dados, comecei o curso por conta de gostar da área e querer se aprofundar cada vez mais no segmento,
-                        aproveitando o crescimento da área e a oportunidade de conhecimento que o Senac proporcionou...
-                        </li>
-                        <br />
-                        <li>Gabriel, 25 anos e nunca doou sangue, certo dia me bateu a curiosidade e coragem para fazer a doação
-                        porem não achava informações faceis de como e onde realizar, então fui atras e descobri que 
-                        não estava apto naquele momento</li>
-                        <br />
-                        <li>Hyan, 25 anos e nunca doou sangue, certo dia me bateu a curiosidade e coragem para fazer a doação
-                        porem não achava informações faceis de como e onde realizar, então fui atras e descobri que 
-                        não estava apto naquele momento.</li>
-                        <br/>
-                        <li>Ivan, 25 anos e nunca doou sangue, certo dia me bateu a curiosidade e coragem para fazer a doação
-                        porem não achava informações faceis de como e onde realizar, então fui atras e descobri que 
-                        não estava apto naquele momento.</li>
-                        <br />
-                        <br />
-                        <div className="sobre_doacao_">
-                        Ajude-nos e Seja um Sanguebom!
->>>>>>> 25d7cdf02f82cfaeaef4d1ce888a76e3b0006d47
                         <br />
                         <h2>Ajude a manter o projeto e seja um membro Sangue Bom!
                         Faça sua doação via PIX para os seguintes dados:</h2>
@@ -77,7 +44,6 @@ function Sobre(){
                         <br />
                         <br />
                             <div className="pai_qr">
-<<<<<<< HEAD
                                 <div className="info_qr"> 
                                 <h2>CNPJ: XXXXXXXXXXXXXXXXXXXX
                                 <br />Nome: XXXXXXXXXXXXXXXXXXXX </h2> </div>
@@ -90,23 +56,6 @@ function Sobre(){
             <br />
             <br />
 
-=======
-                                <div className="info_qr"> <h2>CNPJ: XXXXXXXXXXXXXXXXXXXX <br /> Nome: XXXXXXXXXXXXXXXXXXXX </h2>
-                                </div>
-                                <div className="sobre_qr_">
-                                    <img className="qr_sb" src={QrCode} alt="qr code" />
-                                </div>
-                            </div>
-                        </div>
-                </ul>
-                <h1>Contatos </h1>
-                <br />
-                Email: XXXXXXXXXXXXXXXXXXXXX@EMAIL.COM<br />
-                Telefone: XX XXXXX-XXXX <br />
-                Redes Sociais: @XXXXXXXXXXXX <br />
-            <br />
-            <br />
->>>>>>> 25d7cdf02f82cfaeaef4d1ce888a76e3b0006d47
             <Footer/>
         </div>
     )
