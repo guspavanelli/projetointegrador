8de3c05b8e5f142b345f4f7879d360d35527babe
Gabriel Martielo <gabrielmartielo3214@gmail.com>
2024-10-10 21:57:12 -0300

    Alterações novo cabecalho

--- FRONT/src/pages/erro404.tsx
+++ FRONT/src/pages/erro404.tsx
@@ -5,6 +5,7 @@ import '../style/style.css'
 import ExpandablePopup from "../components/buttons/botaoajuda";
 import Error404 from "../components/erro404";
 import Footer from "../components/footer";
+import Header from "../components/teste";
 
 
 function Erro404(){  
@@ -12,7 +13,7 @@ function Erro404(){
   return (
     <div>
       <div className="home_posso_doar">
-      <Cabecalho/>
+      <Header/>
           <Error404/>
           <BloodTypeSelector/>
           <ExpandablePopup />
