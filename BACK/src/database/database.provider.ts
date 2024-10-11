1bf7f7f36681de9af694e8759ca8293d91307209
Leko <alexandre200970@hotmail.com>
2024-10-10 22:16:53 -0300

    true

--- BACK/src/database/database.provider.ts
+++ BACK/src/database/database.provider.ts
@@ -14,7 +14,7 @@ export const databaseProviders = [
                 entities:[
                     __dirname + '/../**/*.entity{.ts,.js}',
                 ],
-                synchronize: false,
+                synchronize: true,
 
             });
             return dataSource.initialize();
