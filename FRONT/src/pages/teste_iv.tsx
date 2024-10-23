a3575aff2390d28698cc0de89cda8a5c7601b894
1234-ivan <ivandavidchagas@gmail.com>
2024-10-22 22:22:59 -0300

    aa

--- FRONT/src/pages/teste_iv.tsx
+++ FRONT/src/pages/teste_iv.tsx
@@ -86,214 +86,37 @@ function Form_teste() {
     <div className="container">
       <style>
       {`
-    .container-autenticacao {
-      background-color: #fff;
-      border-radius: 10px;
-      box-shadow: 0 0 10px #0003;
-      margin: 8px auto;
-      max-width: 100%;
-      min-height: 480px;
-      overflow: hidden;
-      position: relative;
-      width: 768px;
-    }
+   .container-autenticacao {
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 0 10px #0003;
+    margin: 8px auto;
+    max-width: 100%;
+    min-height: 480px;
+    overflow: hidden;
+    position: relative;
+    width: 768px;
+}
 
-    .logar-conta,
-    .criar-conta {
-      left: 0;
-      width: 50%;
-      position: absolute;
-    }
+.container-autenticacao.active .logar-conta {
+  transform: translateX(100%);
+}
 
-    .logar-conta {
-      z-index: 2;
-    }
+.logar-conta {
+  left: 0;
+  width: 50%;
+  z-index: 2;
+}
 
-    .criar-conta {
-      opacity: 0;
-      z-index: 1;
-    }
-
-    .form-container {
-      height: 100%;
-      top: 0;
-      transition: all .6s ease-in-out;
-    }
-
-    .container-autenticacao form {
-      align-items: center;
-      background-color: #fff;
-      display: flex;
-      flex-direction: column;
-      height: 100%;
-      justify-content: center;
-      padding: 0 20px;
-    }
-
-    .form-container h1 {
-      color: #535353;
-      font-size: 14px;
-    }
-
-    .form-container h1.entrar {
-      position: relative;
-      top: 27px;
-    }
-
-    .form-container img {
-      object-fit: cover;
-      position: relative;
-      top: 15px;
-      width: 240px;
-    }
-
-    .container-input {
-      position: relative;
-    }
-
-    .container-autenticacao input {
-      background-color: #eee;
-      border: 1px solid #0000;
-      border-radius: 8px;
-      font-size: 13px;
-      margin: 3px 0;
-      outline: none;
-      padding: 10px 4px;
-      width: 100%;
-    }
-
-    .container-input img {
-      cursor: pointer;
-      left: 90%;
-      position: absolute;
-      top: 65%;
-      width: 20px;
-    }
-
-    .container-autenticacao button {
-      background-color: #4b8a40;
-      border: 1px solid #0000;
-      border-radius: 10px;
-      color: #fff;
-      cursor: pointer;
-      font-size: 12px;
-      font-weight: 700;
-      letter-spacing: .5px;
-      margin-top: 10px;
-      padding: 10px 45px;
-      text-transform: uppercase;
-    }
-
-    .message-erro, .message-ok {
-      font-size: x-small;
-      margin-top: 2px;
-    }
-
-    a {
-      color: inherit;
-      text-decoration: none;
-    }
-
-    .message-ok {
-      color: #4b8a40;
-      font-weight: 700;
-    }
-
-    .toggle-left {
-      transform: translateX(-200%);
-    }
-
-    .toggle-panel {
-      align-items: center;
-      display: flex;
-      flex-direction: column;
-      height: 100%;
-      justify-content: center;
-      padding: 0;
-      position: absolute;
-      text-align: center;
-      top: 0;
-      transform: translateX(0);
-      transition: all .6s ease-in-out;
-      width: 50%;
-    }
-
-    .container-autenticacao p {
-      font-size: 14px;
-      letter-spacing: .3px;
-      line-height: 20px;
-      margin: 20px 0;
-    }
-
-    .container-autenticacao button.hidden {
-      background-color: initial;
-      border-color: #fff;
-    }
-
-    .toggle-right {
-      right: 0;
-    }
-
-  
-
-    .toggle {
-      background: linear-gradient(90deg, #ff5252, #b71c1c);
-      color: #fff;
-      height: 100%;
-      left: -100%;
-      position: relative;
-      transform: translateX(0);
-      transition: all .6s ease-in-out;
-      width: 200%;
-  }
-    .toggle-container {
-      border-radius: 150px 0 0 100px;
-      height: 100%;
-      left: 50%;
-      overflow: hidden;
-      position: absolute;
-      top: 0;
-      transition: all .6s ease-in-out;
-      width: 50%;
-      z-index: 1000;
-    }
-
-    .container-autenticacao {
-      background-color: #fff;
-      border-radius: 10px;
-      box-shadow: 0 0 10px #0003;
-      margin: 8px auto;
-      max-width: 100%;
-      min-height: 480px;
-      overflow: hidden;
-      position: relative;
-      width: 768px;
-      transition: transform 0.6s ease-in-out; /* Suaviza a transição */
-  }
-  
-  .logar-conta,
-  .criar-conta {
-      left: 0;
-      width: 50%;
-      position: absolute;
-      transition: opacity 0.6s ease-in-out; /* Suaviza a transição da opacidade */
-  }
-  
-  .logar-conta {
-      z-index: 2;
-  }
-  
-  .criar-conta {
-      opacity: 0; /* Inicialmente oculto */
-      z-index: 1;
-  }
-  
-  .form-container {
-      height: 100%;
-      top: 0;
-      transition: all 0.6s ease-in-out;
-  }
-  
+.form-container {
+  height: 100%;
+  position: absolute;
+  top: 0;
+  transition: all .6s ease-in-out;
+}
+ 
+ 
+   
     `}
 </style>
 
