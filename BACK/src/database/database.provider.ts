45c3f779d0bd328ec494695d6cdfd9895cb75dc1
ivan-123 <ivandavidchagas@gmail.com>
2024-10-25 17:15:50 -0300

    aa

--- BACK/src/database/database.provider.ts
+++ BACK/src/database/database.provider.ts
@@ -1,28 +1,3 @@
-// import { DataSource } from "typeorm";
- 
-// export const databaseProviders = [
-//     {
-//         provide: 'DATA_SOURCE',
-//         useFactory: async () => {
-//             const dataSource = new DataSource({
-//                 type: 'mysql',
-//                 host: '50.116.112.16',
-//                 port: 3306,
-//                 username: 'vitali04_sanguebom',
-//                 password: '123Mudar@3',
-//                 database: 'vitali04_db_sanguebom',
-//                 entities:[
-//                     __dirname + '/../**/*.entity{.ts,.js}',
-//                 ],
-//                 synchronize: false,
-               
- 
- 
-//             });
-//             return dataSource.initialize();
-//         },
-//     }
-// ];
 import { DataSource } from "typeorm";
  
 export const databaseProviders = [
@@ -31,15 +6,15 @@ export const databaseProviders = [
         useFactory: async () => {
             const dataSource = new DataSource({
                 type: 'mysql',
-                host: 'localhost',
+                host: '50.116.112.16',
                 port: 3306,
-                username: 'root',
-                password: '',
-                database: 'db_sanguebom',
+                username: 'vitali04_sanguebom',
+                password: '123Mudar@3',
+                database: 'vitali04_db_sanguebom',
                 entities:[
                     __dirname + '/../**/*.entity{.ts,.js}',
                 ],
-                synchronize: true,
+                synchronize: false,
                
  
  
